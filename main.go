18d96467fafacf56272c32c8f10edac04088d389
rdelpret <robbie@lola.com>
2020-08-29 22:31:10 -0600

    We can now decide if we need to create or diff a file, organized, wrote comments

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ import (
 	"time"
 )
 
-// data structure for application
+// --- DATA DEFINITIONS ---
+
 type app struct {
 	Destinations []destination `yaml:"destinations"`
 	Sources      []source      `yaml:"sources"`
@@ -23,11 +24,11 @@ type app struct {
 }
 
 type source struct {
-	Name   string `yaml:"name"`
-	Github string `yaml:"github"`
-	Path   string `yaml:"path"`
+	Name   string   `yaml:"name"`
+	Github string   `yaml:"github"`
+	Path   string   `yaml:"path"`
 	Files  []string `yaml:files`
-	Branch string `yaml:"branch"`
+	Branch string   `yaml:"branch"`
 }
 
 type destination struct {
@@ -42,7 +43,46 @@ type route struct {
 	ObjectTypes []string `yaml:"objectTypes"`
 }
 
-// function that loads the config into the structs
+// --- UTILITY FUNCTIONS ---
+
+// simple function to test if string exists in []string
+func contains(l []string, s string) bool {
+    for _, a := range l {
+        if a == s {
+            return true
+        }
+    }
+    return false
+}
+
+//deletes a given directory
+func cleanup(dir string) {
+	log.Println("cleaning up", dir)
+	err := os.RemoveAll(dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+
+// function that pulls all the file names from
+// a directory and loads them into a slice
+func ls(dir string) []string {
+	var files []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return files[1:]
+}
+
+// --- FILE SYSTEM / YAML DATA FUNCTIONS ---
+
+// loads the config file into structs
+// this will later become a k8s CRD
 func loadAppFile(file string) app {
 
 	var a app
@@ -63,20 +103,6 @@ func loadAppFile(file string) app {
 
 }
 
-// function that pulls all the file names from
-// a directory and loads them into a slice
-func getConfFiles(dir string) []string {
-	var files []string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		files = append(files, path)
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-	return files[1:]
-}
-
 // Downloads the source repo and places it in a temp directory
 func downloadSource(s source) {
 	url := "https://github.com/" + s.Github + "//" + s.Path
@@ -89,56 +115,78 @@ func downloadSource(s source) {
 }
 
 // Downloads a destination repo and puts in a temp directory
-func downloadDestination(d destination) {
+func downloadDestination(d destination) []string {
 	url := "https://github.com/" + d.Github + "//" + d.Path
 	loc := "temp/destinations/" + d.Name
 	err := getter.Get(loc, "git::"+url+"?ref=master")
 	if err != nil {
 		fmt.Println(err)
 	}
-	return
+	return ls(loc)
 }
 
+
+// --- CARTOPGRAPHER APP LOGIC ---
+
 // Main function that process an "app"
-func ProcessApp(file string, num int) {
+func processApp(file string) {
 
 	log.Print("Loading ", file)
 	a := loadAppFile(file)
+	var sourceFileListFromYaml []string
+	var destFileListFromDir []string
 
+  // all processing of sources that does not need awareness of destination
 	for _, s := range a.Sources {
 		log.Printf("Processing Source {name: %s, github: %s, path: %s}\n", s.Name, s.Github, s.Path)
 		log.Println("Downloading source from", "https://github.com/"+s.Github+"//"+s.Path)
-		downloadSource(s)
-	}
 
+		downloadSource(s)
+
+		for _, f := range s.Files {
+			sourceFileListFromYaml = append(sourceFileListFromYaml, f)
+		}
+	}
+  // all processing of destination that does not need awareness of sources
 	for _, d := range a.Destinations {
 		log.Printf("Processing destination {name: %s, github: %s, path: %s}\n", d.Name, d.Github, d.Path)
 		log.Println("Downloading destination from", "https://github.com/"+d.Github+"//"+d.Path)
-		downloadDestination(d)
+
+		downloadedFileList := downloadDestination(d)
+
+		for _, f := range downloadedFileList  {
+			destFileListFromDir = append(destFileListFromDir, f)
+		}
 	}
+
+	// For now assume the file list for each source must appear in each destination
+	// we will need to do some work here to impliment routes but that can be plugged in later
+
+	for _, f := range sourceFileListFromYaml {
+    if !(contains(destFileListFromDir, f)) {
+			log.Println("I must create file", f)
+		} else {
+      log.Println("I must diff file", f)
+		}
+	}
+
+
+
 	return
 }
 
-//cleans up a directory
-func cleanup(dir string) {
-	log.Println("cleaning up", dir)
-	err := os.RemoveAll(dir)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return
-}
+// --- MAIN LOOP ---
 
 func main() {
 	defer cleanup("temp/")
 	for {
-		files := getConfFiles("apps/")
+		files := ls("apps/")
 		log.Printf("Found %d application(s) in /app to process\n", len(files))
-		for i, f := range files {
-			ProcessApp(f, i+1)
+		for _, f := range files {
+			processApp(f)
 		}
 		cleanup("temp/")
-		time.Sleep(5 * time.Second)
+		time.Sleep(60 * time.Second)
 	}
 
 }
